ed55d6bb15d4b990f63cfb10728c41b618d930ed
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-21 14:36:24 +0200

    try to dynamically invoke method in swift

--- sUnit/main.swift
+++ sUnit/main.swift
@@ -1,14 +1,24 @@
 import Foundation
 
-class WasRun {
+class WasRun:NSObject {
     var wasRun = false
+    let name:String
+    
     init(name:String){
+        self.name = name
     }
-    func testMethod(){
+    
+    func testMethod(object: AnyObject?){
         wasRun = true
     }
+    
     func run(){
-        self.testMethod()
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        dispatch_async(queue) {
+            let nameForSelector = self.name + ":"
+            let selector = Selector(nameForSelector)
+            NSThread(target: self , selector: selector, object: nil).start()
+        }
     }
 }
 
