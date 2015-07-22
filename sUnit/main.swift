48fcc9c355bc54c208d641a6888274ba99d6e152
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-22 14:08:11 +0200

    invoke test method by sending it as a closure

--- sUnit/main.swift
+++ sUnit/main.swift
@@ -1,28 +1,26 @@
 import Foundation
 
-class WasRun:NSObject {
-    var wasRun = false
-    let name:String
-    
-    init(name:String){
-        self.name = name
-    }
-    
-    func testMethod(object: AnyObject?){
-        wasRun = true
-    }
-    
-    func run(){
-        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-        dispatch_async(queue) {
-            let nameForSelector = self.name + ":"
-            let selector = Selector(nameForSelector)
-            NSThread(target: self , selector: selector, object: nil).start()
-        }
+class WasRun {
+    func testMethod(){
     }
 }
 
-let test = WasRun(name: "testMethod")
+class TestCase {
+    var wasRun = false
+
+    let method:Void->()
+    
+    init(method:Void->()){
+        self.method = method
+    }
+    
+    func run(){
+        wasRun = true
+        method()
+    }
+}
+
+let test = TestCase(method: WasRun().testMethod)
 println(test.wasRun)
 test.run()
 println(test.wasRun)
